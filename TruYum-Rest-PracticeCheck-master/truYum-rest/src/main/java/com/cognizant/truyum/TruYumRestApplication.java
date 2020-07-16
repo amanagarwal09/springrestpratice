850e8035aba57937a952c5d67f2beab7bfb595a8
amanagarwal09 <62548194+amanagarwal09@users.noreply.github.com>
2020-07-16 14:38:40 +0530

    Add files via upload

---
+++ TruYum-Rest-PracticeCheck-master/truYum-rest/src/main/java/com/cognizant/truyum/TruYumRestApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.truyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruYumRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruYumRestApplication.class, args);
+	}
+
+}
